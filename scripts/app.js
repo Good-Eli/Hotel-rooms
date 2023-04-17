d9e55954c6a2dae76d6714c16def881e5b9a6c7d
Виталий Викторович <v.sioffo@gmail.com>
2023-04-18 01:14:22 +0300

    modified:   scripts/app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,26 @@
-const buttons = document.querySelectorAll('.btn');
-buttons.forEach((button, index) => {
-  button.addEventListener('click', function () {
-    const div = document.querySelectorAll('.cover-reserved')[index];
-    div.style.display = 'grid';
+const rooms = document.querySelectorAll('.room');
+
+rooms.forEach((room) => {
+  const button = room.querySelector('.btn');
+
+  button.addEventListener('click', () => {
+    button.disabled = true;
+    button.textContent = 'Обработка...';
+
+    const cover = room.querySelector('.cover');
+    const coverReserved = room.querySelector('.cover-reserved');
+
+    const handleMouseLeave = () => {
+      cover.removeEventListener('mouseleave', handleMouseLeave);
+      coverReserved.style.display = 'grid';
+      button.textContent = 'Забронировать';
+      button.disabled = false;
+    };
+
+    cover.addEventListener('mouseleave', handleMouseLeave);
   });
 });
+
 
 function hideElement(element) {
   element.style.display = 'none';
